dbc6c0fb29b86c1bb198f4a066278c29a78630d6
Toktam <ttmohebbi@gmail.com>
2020-10-28 01:51:05 -0400

    refactor the Button component so that it uses the classnames library to create a buttonClass string

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,14 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classnames from "classnames";
+
+
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   const buttonClass = classnames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
 
    return (
       <button
@@ -23,5 +21,7 @@ export default function Button(props) {
       </button>
    );
 }
+
+
 
 
