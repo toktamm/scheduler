e124865a46766d880d6e47ae54fa5ad791a37f5e
Toktam <ttmohebbi@gmail.com>
2020-10-28 03:51:24 -0400

    populating the DayList component and  map over the days array

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,27 +3,22 @@ import React from 'react';
 import DayListItem from 'components/DayListItem';
 
 export default function DayList(props) {
-
-  return (
-    <ul>
-      <DayListItem
-        name={"Monday"}
-        spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
-      />
-      <DayListItem
-        name={"Tuesday"}
-        spots={day.spots}
-        selected={day.name === props.day}
-        setDay={props.setDay}
-      />
-
-      ...
-
-    </ul>
-  )
+  return <ul>
+    {props.days.map((day) => {
+      return (
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay}
+        />
+      )
+    })}
+  </ul>
 }
+
+
 
 
 
