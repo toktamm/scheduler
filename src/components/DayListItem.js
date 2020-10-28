94ff3873a5ff7025e5b2a2fc7fd3397ede96fd37
Toktam <ttmohebbi@gmail.com>
2020-10-28 03:23:02 -0400

    dayListItem update to apply styles based on the props we are passing to DayListItem using the classnames library

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,27 @@
 import React from "react";
 
+import classnames from "classnames";
+
+import "components/DayListItem.scss";
+
 export default function DayListItem(props) {
+
+  const dayClass = classnames(
+    "day-list__item",
+    {
+      "day-list__item--selected": props.selected,
+      "day-list__item--full": props.spots === 0
+    }
+  )
+
+  function formatSpots() {
+    return props.spots === 0 ? 'no spots remaining' : `${props.spots} spot${props.spots === 1 ? '' : 's'} remaining`
+  }
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li key={props.id} className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
