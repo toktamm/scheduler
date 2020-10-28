f9d497e9b6bcb5eddb6518e9379e92324ccfb8ff
Toktam <ttmohebbi@gmail.com>
2020-10-27 21:38:04 -0400

    add the conditional class names logic to the Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,12 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <button>{props.children}</button>;
+   let buttonClass = "button";
+
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+   }
+
+   return <button className={buttonClass}>{props.children}</button>;
 }
+
