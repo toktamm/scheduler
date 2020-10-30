b9c6fe440997e9c17c77045469a97c9a7232768a
Toktam <ttmohebbi@gmail.com>
2020-10-30 19:25:23 -0400

    refactor getAppointmentsForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,19 +1,26 @@
 export function getAppointmentsForDay(state, day) {
   //  get object for a particular day
   const specificDay = state.days.find(item => item.name === day)
-  // console.log("specificDay: ", specificDay);
-  // console.log("appointments: ", state.appointments);
 
-  if(!specificDay) {
+  if (!specificDay) {
     return [];
   }
   // get appointments array
   //  match appointment ids and return appointments data array
-  const appointmentsData = specificDay.appointments.map(item => {
-    // console.log("item: ", item);
-    return state.appointments[item];
-  })
-  // console.log("appointmentsData: ", appointmentsData);
+  const appointmentsData = specificDay.appointments.map(item => state.appointments[item])
+
   return appointmentsData;
+  
 }
+
+
+/* alternative solution
+
+export function getAppointmentsForDay(state, day) {
+  const filteredDay = state.days.filter(elem => elem.name === day)[0]
+  const appointments = filteredDay ? filteredDay.appointments.map(elem => state.appointments[elem]) : [];
+  return appointments;
+}
+
+*/
 
