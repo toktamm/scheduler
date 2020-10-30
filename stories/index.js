fa20c2404fa06c0db3efe0384db8ac11d09b90cd
Toktam <ttmohebbi@gmail.com>
2020-10-30 01:33:56 -0400

    update the Appointment component to display appointments

--- stories/index.js
+++ stories/index.js
@@ -155,7 +155,22 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Appointment Empty", () => (
+    <>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </>
+  ))
+  .add("Appointment Booked", () => (
+    <>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </>
+  ))  
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => <Show student='Lydia Miller-Jones' interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
