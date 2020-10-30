27541cd019aefb3ffe77842f2c4f22a7c398148c
Toktam <ttmohebbi@gmail.com>
2020-10-30 17:24:16 -0400

    add axios and get data from scheduler-api server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 
 import DayList from "components/DayList";
 import Appointment from 'components/Appointment';
+
+import axios from 'axios';
 
 
 
@@ -55,27 +57,38 @@ const appointments = [
 ];
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/days')
+      .then(res => {
+        setDays([...res.data]);
+      })
+  }, []);
+
 
 
   return (
