abe62ba8edcfe5116e4246b35e14be5622beeb33
Toktam <ttmohebbi@gmail.com>
2020-10-29 22:15:23 -0400

    create the Show component and its story

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show'
+
 
 
 
@@ -148,7 +150,9 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student='Lydia Miller-Jones' interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />) ;
+  
 
 
 
