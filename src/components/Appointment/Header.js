e463cc569aa346c11d99ed1513c5a3b8f617b064
Toktam <ttmohebbi@gmail.com>
2020-10-29 22:50:12 -0400

    fix the Header component

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = (props) => {
+export default function Header(props) {
   return (
     <header className="appointment__time">
       <h4 className="text--semi-bold">{props.time}</h4>
