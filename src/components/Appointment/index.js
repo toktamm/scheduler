ffb53dd74e16a3c3df7048f32e454f8527a1764b
Toktam <ttmohebbi@gmail.com>
2020-11-02 23:44:06 -0500

    custom hook for different states for the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,41 @@ import React from 'react';
 import './styles.scss';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
-import Empty from 'components/Appointment/Empty';
+import Empty from 'components/Appointment/Empty'; import Form from 'components/Appointment/Form';
+import useVisualMode from '../../hooks/useVisualMode'
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
+
 
 
 
 export default function Appointment(props) {
 
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
-        < Show student={props.interview.student} interviewer={props.interview.interviewer} /> :
-        < Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onEdit={() => console.log("Clicked onEdit")}
+          onDelete={() => console.log("Clicked onDelete")}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => console.log("Clicked onSave")}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   )
 
