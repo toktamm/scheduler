304a503cd2bddd9f2eeaba4b7ec850a7f5884f51
Toktam <ttmohebbi@gmail.com>
2020-11-04 01:10:23 -0500

    update spots remaining.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
+import { getAppointmentsForDay } from '../helpers/selectors'
+
+
 
 export default function useApplicationData() {
 
@@ -10,51 +13,6 @@ export default function useApplicationData() {
     appointments: {},
     interviewers: {}
   });
-
-
-  function bookInterview(id, interview) {
-
-    // copying appointment object at particular id
-    // replacing interview null to interview with data
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-
-    // copying appointmentS object and updating with new appointment data
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios
-      .put(`/api/appointments/${id}`, appointment)
-      .then(() => {
-        setState({
-          ...state,
-          appointments
-        })
-      })
-  }
-
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-    return axios
-      .delete(`/api/appointments/${id}`)
-      .then(() => {
-        setState({
-          ...state,
-          appointments
-        })
-      })
-  }
 
 
   // setDay function can remain because we are only using it to update our DayList component
@@ -74,6 +32,60 @@ export default function useApplicationData() {
       }))
     })
   }, []);
+
+
+  // to calculate how many spots are left, we need to check how many interviews are set to null
+  // for each day and return a new days array
+  const availableSpots = state => {
+    const newState = { ...state }
+    const days = newState.days.map(day => {
+      const spots = getAppointmentsForDay(newState, day.name).filter(appointment => appointment.interview === null).length;
+      return { ...day, spots }
+    })
+    return days;
+  }
+
+  
+
+  function bookInterview(id, interview) {
+
+    // copying appointment object at particular id
+    // replacing interview null to interview with data
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    // copying appointmentS object and updating with new appointment data
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios
+      .put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState(prev => ({ ...prev, appointments }))
+        setState(prev => ({ ...prev, days: availableSpots(prev) }))
+      })
+  }
+
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    }
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+    return axios
+      .delete(`/api/appointments/${id}`)
+      .then(() => {
+        setState(prev => ({ ...prev, appointments }))
+        setState(prev => ({ ...prev, days: availableSpots(prev) }))
+      })
+  }
 
 
   return {
