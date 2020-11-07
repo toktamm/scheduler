ff1d7cb75fea60521b1a3776ac0f079f952f47f9
Toktam <ttmohebbi@gmail.com>
2020-11-07 00:50:29 -0500

    add should cancel interview test, the previous commit was for editing tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -41,4 +41,21 @@ describe("Appointments", () => {
   });
 
 
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .click({ force: true });
+
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+  });
+
+
+
+
 });
