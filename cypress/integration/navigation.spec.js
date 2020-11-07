4f1517dda20a1334a67e7db26060e89511be1022
Toktam <ttmohebbi@gmail.com>
2020-11-07 00:03:59 -0500

    add first Cypress test for CSS background color

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,3 +3,15 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 });
+
+
+it("should navigate to Tuesday", () => {
+  cy.visit("/");
+
+  cy.contains("li", "Tuesday")
+  .click()
+  .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+});
+
+
